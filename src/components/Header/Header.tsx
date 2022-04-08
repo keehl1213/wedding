48537ee55cd864e9140d580f4b15d948dbad6854
Nina Tang <nina_tang@wistron.com>
2022-04-09 00:29:09 +0800

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Header.sass';
+
+const Header: React.FC = () => <div className="header" />;
+
+export default Header;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,32 +1,11 @@
-import React, { useEffect, useContext } from 'react';
-// import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
-// import { getUserInfoApi } from 'api/tenantAccountApi';
-// import { logoutApi } from 'api/loginApi';
-// import { LoadingBar, IftttHeader } from 'component';
-// import { GlobalContext } from 'store/appStore';
-// import { startTimer, stopTimer } from 'utils/tokenHelper';
-// import { apiCaller } from 'service/apiCaller';
+import React from 'react';
 import { FullpageRoutes } from 'router';
-
-// const Main = styled.div`
-//     position: relative;
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: flex-start;
-//     align-items: center;
-//     width: 100vw;
-//     height: calc(100vh - 80px);
-//     overflow: hidden;
-// `;
+import Header from 'components/Header/Header';
 
 const App: React.FC<any> = () => (
-    <>
-        {/* <LoadingBar /> */}
-        {/* <IftttHeader /> */}
-        <div className="main">
-            <FullpageRoutes />
-        </div>
-    </>
+    <div className="main">
+        <Header />
+        <FullpageRoutes />
+    </div>
 );
 export default App;
