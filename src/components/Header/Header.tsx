63375e5f4d1713c5c6b41fb3bb4e21a917be9d85
Nina Tang <nina_tang@wistron.com>
2022-04-09 12:07:58 +0800

    Upload photos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.sass';
 
-const Header: React.FC = () => <div className="header" />;
+const Header: React.FC = () => <div className="header">哲瑜 & 雯倩</div>;
 
 export default Header;
