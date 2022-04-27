913dd052024d7ffebd32f830630e8c473bdd628f
Nina Tang <nina_tang@wistron.com>
2022-04-27 23:51:09 +0800

    Update story photos

--- src/assets/storys/index.js
+++ src/assets/storys/index.js
@@ -1,6 +1,29 @@
+import storyClimbing from './story_climbing.jpg';
+import storyDiving from './story_diving.jpg';
 import storyFailed from './story_failed.MOV';
+import storyFailedPreview from './story_failed_preview.jpg';
+import storyFirstAbroad from './story_first_abroad.mov';
+import storyFirstAbroadPreview from './story_first_abroad_preview.JPG';
 import storyFirstDate from './story_first_date.jpg';
 import storyFirstLook from './story_first_look.jpg';
 import storyFlower from './story_flower.jpg';
+import storyHousewarmingParty from './story_housewarming_party.jpg';
+import storyMarried from './story_married.jpg';
+import storyPet from './story_pet.JPG';
+import storyPropose from './story_propose.jpg';
 
-export { storyFailed, storyFirstDate, storyFirstLook, storyFlower };
+export {
+    storyClimbing,
+    storyDiving,
+    storyFailed,
+    storyFailedPreview,
+    storyFirstAbroad,
+    storyFirstAbroadPreview,
+    storyFirstDate,
+    storyFirstLook,
+    storyFlower,
+    storyHousewarmingParty,
+    storyMarried,
+    storyPet,
+    storyPropose,
+};
--- src/containers/OurStory/OurStory.tsx
+++ src/containers/OurStory/OurStory.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from 'react';
+import React, { useMemo } from 'react';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -31,7 +31,13 @@ const storyList = [
     {
         date: 'December 17 ,2018',
         content: '第一次告白失敗',
-        media: <video controls src={Storys.storyFailed} />,
+        media: (
+            <video
+                controls
+                src={Storys.storyFailed}
+                poster={Storys.storyFailedPreview}
+            />
+        ),
     },
     {
         date: 'December 23 ,2018',
@@ -59,39 +65,140 @@ const storyList = [
             />
         ),
     },
+    {
+        date: 'December 4, 2019',
+        content: '第一次一起出國',
+        media: (
+            <video
+                controls
+                src={Storys.storyFirstAbroad}
+                poster={Storys.storyFirstAbroadPreview}
+            />
+        ),
+    },
+    {
+        date: 'Someday',
+        content: '一起爬山',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyClimbing}
+                src={Storys.storyClimbing}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="climbing" />
+                )}
+            />
+        ),
+    },
+    {
+        date: 'Someday',
+        content: '一起玩水',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyDiving}
+                src={Storys.storyDiving}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="diving" />
+                )}
+            />
+        ),
+    },
+    {
+        date: 'August 22, 2021',
+        content: '一起養寵物',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyPet}
+                src={Storys.storyPet}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="pet" />
+                )}
+            />
+        ),
+    },
+    {
+        date: 'October 14, 2021',
+        content: '求婚',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyPropose}
+                src={Storys.storyPropose}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="propose" />
+                )}
+            />
+        ),
+    },
+    {
+        date: 'March, 2022',
+        content: '遲了一年的新家入厝',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyHousewarmingParty}
+                src={Storys.storyHousewarmingParty}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="housewarming" />
+                )}
+            />
+        ),
+    },
+    {
+        date: 'Future',
+        content: '未完待續',
+        media: (
+            <ProgressiveImage
+                preview={Storys.storyMarried}
+                src={Storys.storyMarried}
+                render={(src, style) => (
+                    <img src={src} style={style} alt="married" />
+                )}
+            />
+        ),
+    },
 ];
 
 const OurStory: React.FC = () => {
     const [width] = useResize();
+    const isComputer = width > 640;
+    const timelineComp = useMemo(
+        () => (
+            <Timeline position="alternate">
+                {storyList.map((story) => (
+                    <TimelineItem key={uuidv4()}>
+                        <TimelineOppositeContent color="text.secondary">
+                            <h2 className="story-date">{story.date}</h2>
+                            <div className="story-content">{story.content}</div>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot />
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent>{story.media}</TimelineContent>
+                    </TimelineItem>
+                ))}
+            </Timeline>
+        ),
+        [isComputer]
+    );
+    const sTimelineComp = useMemo(
+        () =>
+            storyList.map((story) => (
+                <Divider key={uuidv4()} orientation="vertical" flexItem>
+                    {story.media}
+                    <h2 className="story-date">{story.date}</h2>
+                    <div className="story-content">{story.content}</div>
+                </Divider>
+            )),
+        [isComputer]
+    );
     return (
         <div className="our-story">
-            {width > 640 ? (
-                <Timeline position="alternate">
-                    {storyList.map((story) => (
-                        <TimelineItem key={uuidv4()}>
-                            <TimelineOppositeContent color="text.secondary">
-                                <h2 className="story-date">{story.date}</h2>
-                                <div className="story-content">
-                                    {story.content}
-                                </div>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineDot />
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent>{story.media}</TimelineContent>
-                        </TimelineItem>
-                    ))}
-                </Timeline>
-            ) : (
-                storyList.map((story) => (
-                    <Divider key={uuidv4()} orientation="vertical" flexItem>
-                        {story.media}
-                        <h2 className="story-date">{story.date}</h2>
-                        <div className="story-content">{story.content}</div>
-                    </Divider>
-                ))
-            )}
+            {isComputer ? timelineComp : sTimelineComp}
+            {/* <iframe
+                title="memory"
+                src="https://www.google.com/maps/d/embed?mid=195y3R6S2crqow3QRY1m-BAHAwl507ZJO&ehbc=2E312F"
+                width="640"
+                height="480"
+            /> */}
         </div>
     );
 };
